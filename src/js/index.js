c4f69ce215dfe0cb26b974d708a68eabd72f6a87
axelduch <axel@procheo.fr>
2014-05-19 00:20:38 +0200

    Updating loaded stuff inside index

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,27 @@
 'use strict';
 
 var PIXI = require('pixi'),
-    assets = require('./assets.js'),
-    Hero = require('./hero.js'),
+    assets = require('./assets'),
+    Camera = require('./Camera'),
+    Hero = require('./Hero'),
+    input = require('./input'),
     stage,
     renderer,
     hero,
     texture,
-    width = 800,
-    height = 600,
+    width = 500,
+    height = 500,
     animate,
     loader = new PIXI.AssetLoader(assets.array),
-    requestAnimationFrame = window.requestAnimationFrame;
+    requestAnimationFrame = window.requestAnimationFrame,
+    game;
 
 loader.onComplete = onAssetsLoaded;
 loader.load();
 
 function onAssetsLoaded() {
+    var camera,
+        hero;
     console.log('loaded');
     stage = new PIXI.Stage(0xAAFF);
     renderer = PIXI.autoDetectRenderer(width, height);
@@ -25,18 +30,25 @@ function onAssetsLoaded() {
 
     requestAnimationFrame(animate);
 
-    var hero = new Hero();
+    hero = new Hero();
     hero.width = 100;
     hero.height = 100;
 
     hero.anchor.x = 0.5;
     hero.anchor.y = 0.5;
 
-
     hero.position.x = width >> 1;
     hero.position.y = height >> 1;
 
+    camera = new Camera();
     stage.addChild(hero);
+
+    game = {
+        hero: hero,
+        stage: stage
+    };
+
+    input(game);
 }
 
 function animate() {
