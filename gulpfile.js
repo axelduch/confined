35c359c51b4a5264519f67bbbcfe4550345465d5
axelduch <axel@procheo.fr>
2014-05-19 00:17:54 +0200

    Reorganize tasks and add jshint

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
     watch = require('gulp-watch'),
     browserify = require('gulp-browserify'),
     livereload = require('gulp-livereload'),
+    jshint = require('gulp-jshint'),
     paths;
 
 paths = {
@@ -9,11 +10,21 @@ paths = {
     'html': './src/index.html'
 };
 
+gulp.task('jshint', function () {
+    gulp.src(paths.scripts)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
 gulp.task('browserify', function () {
     gulp.src(paths.scripts)
-        .pipe(browserify().on('error', function (e) { console.log('Browserify has failed', e); }))
-        .pipe(livereload())
+        .pipe(browserify().on('error', function (e) { console.log('Browserify has failed'); }))
         .pipe(gulp.dest('./dist/js'))
+});
+
+gulp.task('livereload', function () {
+    gulp.src(paths.scripts)
+        .pipe(livereload());
 });
 
 gulp.task('index.html', function () {
@@ -22,6 +33,6 @@ gulp.task('index.html', function () {
 });
 
 gulp.task('default', function () {
-    gulp.watch(paths.html, ['index.html']);
-    gulp.watch(paths.scripts, ['browserify']);
+    gulp.watch(paths.html, ['index.html', 'livereload']);
+    gulp.watch(paths.scripts, ['browserify', 'livereload', 'jshint']);
 });
