c5125f9d220de65446d1c44c4d7c6bbfd0c924bd
axelduch <axel@procheo.fr>
2014-05-19 00:19:46 +0200

    Adding attack method to hero

--- src/js/hero.js
+++ src/js/hero.js
@@ -6,13 +6,36 @@ var PIXI = require('pixi'),
 
 Hero = function Hero() {
     var textures = [
-        PIXI.Texture.fromImage(assets.hash['hero.png'])
+        PIXI.Texture.fromImage(assets.hash['hero.png']),
+        PIXI.Texture.fromImage(assets.hash['hero_attack.png'])
     ];
     PIXI.MovieClip.call(this, textures);
+    this.speed = 10;
 };
 
 Hero.prototype = Object.create(PIXI.MovieClip.prototype, {
     constructor: Hero
 });
+
+Hero.prototype.walkRight = function () {
+    this.position.x += this.speed;
+};
+
+Hero.prototype.walkLeft = function () {
+    this.position.x -= this.speed;
+};
+
+Hero.prototype.walkUp = function () {
+    this.position.y -= this.speed;
+};
+
+Hero.prototype.walkDown = function () {
+    this.position.y += this.speed;
+};
+
+Hero.prototype.attack = function () {
+    this.gotoAndStop(1);
+};
+
 
 module.exports = Hero;
