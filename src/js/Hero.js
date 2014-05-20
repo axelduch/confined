715c13b9f8096c5a83933431fd37c581eed9ae78
Axel Duch <axlduch@gmail.com>
2014-05-21 00:55:07 +0200

    Use Vector with Hero

--- src/js/Hero.js
+++ src/js/Hero.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var PIXI = require('pixi'),
+    Vector = require('./physics/Vector.js'),
     assets = require('./assets.js'),
     Hero;
 
@@ -9,33 +10,31 @@ Hero = function Hero() {
         PIXI.Texture.fromImage(assets.hash['hero.png']),
         PIXI.Texture.fromImage(assets.hash['hero_attack.png'])
     ];
+
     PIXI.MovieClip.call(this, textures);
-    this.speed = 10;
+
+    this.acceleration = new Vector();
 };
 
 Hero.prototype = Object.create(PIXI.MovieClip.prototype, {
     constructor: Hero
 });
 
-Hero.prototype.walkRight = function () {
-    this.position.x += this.speed;
-};
+Hero.prototype.move  = function (game) {
+    this.position.x += this.acceleration.x;
 
-Hero.prototype.walkLeft = function () {
-    this.position.x -= this.speed;
-};
-
-Hero.prototype.walkUp = function () {
-    this.position.y -= this.speed;
-};
-
-Hero.prototype.walkDown = function () {
-    this.position.y += this.speed;
+    if (game.IS_JUMPING) {
+        this.position.y += this.acceleration.y;
+    }
 };
 
 Hero.prototype.attack = function () {
     this.gotoAndStop(1);
 };
 
+Hero.prototype.update = function (game) {
+    this.move(game);
+    this.acceleration.x = 0;
+};
 
 module.exports = Hero;
