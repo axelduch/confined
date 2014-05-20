f3ffe713f91c5197c6317c4afbfa7849ec594d41
Axel Duch <axlduch@gmail.com>
2014-05-21 00:56:58 +0200

    Game loop with new mechanics for index

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,8 @@ var PIXI = require('pixi'),
     assets = require('./assets'),
     Camera = require('./Camera'),
     Hero = require('./Hero'),
+    Ground = require('./Ground'),
+    Vector = require('./physics/Vector'),
     input = require('./input'),
     stage,
     renderer,
@@ -21,7 +23,10 @@ loader.load();
 
 function onAssetsLoaded() {
     var camera,
-        hero;
+        hero,
+        ground,
+        gravity;
+
     console.log('loaded');
     stage = new PIXI.Stage(0xAAFF);
     renderer = PIXI.autoDetectRenderer(width, height);
@@ -30,7 +35,7 @@ function onAssetsLoaded() {
 
     requestAnimationFrame(animate);
 
-    hero = new Hero();
+    hero = new Hero(game);
     hero.width = 100;
     hero.height = 100;
 
@@ -40,18 +45,48 @@ function onAssetsLoaded() {
     hero.position.x = width >> 1;
     hero.position.y = height >> 1;
 
+    ground = new Ground();
+    ground.width = width;
+    ground.position.x = 0;
+    ground.position.y = height - ground.height;
+
     camera = new Camera();
+    stage.addChild(ground);
     stage.addChild(hero);
 
+    gravity = new Vector(0, 0.05);
+
     game = {
+        stage: stage,
         hero: hero,
-        stage: stage
+        ground: ground,
+        physics: {
+            forces: [gravity],
+            forcesSubscribers: [hero]
+        },
+        toUpdate: [hero],
+        IS_JUMPING: true
     };
 
     input(game);
 }
 
 function animate() {
+    var a1, a2, i, j, k, l;
+
+    // apply each force to each force subscriber
+    for (i = 0, a1 = game.physics.forces, l = a1.length; i < l; ++i) {
+        for (j = 0, a2 = game.physics.forcesSubscribers, k = a2.length; j < k; ++j) {
+            a2[j].acceleration.add(a1[i]);
+        }
+    }
+
+    for (i = 0, a1 = game.toUpdate, l = a1.length; i < l; ++i) {
+        a1[i].update(game);
+    }
+
+    game.IS_JUMPING = ((game.hero.position.y - game.hero.height * 0.5) < game.ground.hitArea.y);
+
     requestAnimationFrame(animate);
     renderer.render(stage);
-};
+}
