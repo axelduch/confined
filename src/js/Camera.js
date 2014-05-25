c27771f1b6310c613d3a44057ff9dd062529f4c0
axelduch <axel@procheo.fr>
2014-05-25 23:22:38 +0200

    Greatly improved camera boundaries, still buggy though

--- src/js/Camera.js
+++ src/js/Camera.js
@@ -11,7 +11,7 @@ Camera = function Camera(game) {
         width = game.width,
         height = game.height;
 
-    this.focus = 3;
+    this.focus = 2.5;
     this.game = game;
     this.x = width * 0.5;
     this.y = height * 0.5;
@@ -24,11 +24,22 @@ Camera = function Camera(game) {
             return scale;
         },
         set: function (value) {
+            var scenePreviousX = scene.position.x;
+
             scale = value;
+
             scene.scale.x = scale;
             scene.scale.y = scale;
+
             scene.position.x = this.x - game.hero.position.x * scene.scale.x;
             scene.position.y = this.y - game.hero.position.y * scene.scale.y;
+            console.clear();
+            if (scene.position.x * scene.scale.x > 0) {
+                scene.position.x = 0;
+            }
+            if ((scene.position.x - this.x + game.hero.width) * scene.scale.x < -width * 2 * scene.scale.x) {
+                scene.position.x = scenePreviousX;
+            }
         }
     });
     this.scene = scene;
@@ -44,10 +55,15 @@ Camera.prototype.notify = function notify() {
         scrollIteration: 0
     };
     var c = notify.cache,
+        game = this.game,
+        scene = game.scene,
+        width = scene.width,
+        height = scene.height,
         distScroll = 10000 - c.scrollIteration,
         distZoom = 10000 - c.zoomIteration;
-    this.x = Math.cos((c.scrollIteration += (distScroll * 0.000009)) * 0.05) * this.game.hero.position.x * 0.2 + this.game.width * 0.5;
-    this.scale = 0.2 * Math.abs(Math.sin((c.zoomIteration = (c.zoomIteration + distZoom * 0.000002)) * 0.5)) + this.focus;
+
+    this.x = Math.cos((c.scrollIteration = (c.scrollIteration + distScroll * 0.000009) % 10000) * 0.05) * this.game.hero.position.x * 0.1 + this.game.width * 0.5;
+    this.scale = 0.2 * Math.abs(Math.sin((c.zoomIteration = (c.zoomIteration + distZoom * 0.000002) % 10000) * 0.5)) + this.focus;
 };
 
 module.exports = Camera;
